c2ae5c7fd2b2001639f4af2b67a221646c6bb433
hurricanmarkus <markus.zika@gmail.com>
2025-10-24 08:47:20 +0200

    push

--- pushbenachrichtigung.js
+++ pushbenachrichtigung.js
@@ -1,6 +1,6 @@
-const PUSHOVER_TOKEN = 'aeqwab5g325hvmk33gtq7s3bz8z7x6';
+export const PUSHOVER_TOKEN = 'aeqwab5g325hvmk33gtq7s3bz8z7x6';
 
-const RECIPIENT_KEYS = {
+export const RECIPIENT_KEYS = {
     Markus: 'ue2n25mzqfjnt5mwxkzgik35xotswi',
     Jasmin: 'ugag4tcjpdpq6cfca5ezkrav9pn2va',
     ALLE: 'gaqm7tzrvqxmkjsd8a1t53cvghdog8'
