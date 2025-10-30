46cd2de4d695b9f8e00fbe88693bbbe39c79411d
hurricanmarkus <markus.zika@gmail.com>
2025-10-30 08:41:48 +0100

    adminrollen

--- admin_benutzersteuerung.js
+++ admin_benutzersteuerung.js
@@ -1,10 +1,10 @@
 // BEGINN-ZIKA: IMPORT-BEFEHLE IMMER ABSOLUTE POS1 // TEST 2
 import { onSnapshot, doc, updateDoc, setDoc, deleteDoc, getDoc } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
-import { db, usersCollectionRef, setButtonLoading, checkCurrentUserValidity, adminSectionsState, rolesCollectionRef, ROLES, roleChangeRequestsCollectionRef, currentUser, alertUser, USERS, initialAuthCheckDone, modalUserButtons, ADMIN_ROLES, ADMIN_STORAGE_KEY } from './haupteingang.js';
+import { db, usersCollectionRef, setButtonLoading, adminSectionsState, rolesCollectionRef, ROLES, roleChangeRequestsCollectionRef, currentUser, alertUser, USERS, initialAuthCheckDone, modalUserButtons, ADMIN_ROLES, ADMIN_STORAGE_KEY } from './haupteingang.js';
 import { logAdminAction } from './admin_protokollHistory.js';
 import { setupPermissionDependencies, renderAdminRightsManagement } from './admin_rechteverwaltung.js'; // Oder der richtige Dateiname
 import { restoreAdminScrollIfAny, rememberAdminScroll } from './admin_adminfunktionenHome.js';
-import { updateUIForMode, switchToGuestMode } from './log-InOut.js';
+import { checkCurrentUserValidity, updateUIForMode, switchToGuestMode } from './log-InOut.js';
 // ENDE-ZIKA //
 
 
